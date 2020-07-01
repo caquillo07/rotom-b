c0fbcf6ab3ded1d80d10938ac563b1e711aa4b9d
lewymd <lewdavies@gmail.com>
2020-07-01 12:27:16 +0100

    remove personal mod and .bat file

--- bot/command.go
+++ bot/command.go
@@ -68,7 +68,7 @@ func (b *Bot) newEmbed() *discordgo.MessageEmbed {
 	return &discordgo.MessageEmbed{
 		Color: b.config.Bot.EmbedColor,
 		Footer: &discordgo.MessageEmbedFooter{
-			Text:    "Rotom-B - By Hector & Milla - Modified by Lewymd",
+			Text:    "Rotom-B - By Hector & Milla",
 			IconURL: "https://images-na.ssl-images-amazon.com/images/I/41x0Y9yJYKL.jpg",
 		},
 	}
