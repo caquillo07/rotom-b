b20000043f4611d2b718b689756e5d21226e19c5
Hector Mejia <caquillo722@gmail.com>
2020-07-02 00:26:50 -0400

    halfway through top4 on catch command

--- bot/cmd_catch.go
+++ bot/cmd_catch.go
@@ -23,47 +23,70 @@ func (b *Bot) handleCatchCmd(
 
 	isShiny := strings.HasSuffix(env.args[0], "*") || strings.HasPrefix(env.args[0], "*")
 	cleanPkmName := strings.ReplaceAll(env.args[0], "*", "")
-	var embed *discordgo.MessageEmbed
-	var err error
+	form := getFormFromArgs(env.args)
+	ball, err := b.pokemonRepo.ball(env.args[len(env.args)-1])
+	if err != errBallDoesNotExist {
+		return err
+	}
 
-	// we just got a pokemon request
-	if len(env.args) == 1 {
-		embed, err = b.getPokemonTopFourBalls(cleanPkmName, isShiny)
+	// now make sure that someone did not just send in a ball and no pokemon
+	if ball != nil && len(env.args) == 1 {
+		return botError{
+			title:   "Validation Error",
+			details: "A pokemon must be provided alongside the PokeBall for the catch command",
+		}
+	}
+
+	// If the ball does not exist, that means we got just a pokemon request
+	if ball == nil {
+		embed, err := b.getPokemonTopFourBalls(cleanPkmName, form, isShiny)
 		if err != nil {
 			return err
 		}
-	} else if len(env.args) == 2 {
-		// we got a pokemon and ball request
-
+		_, err = s.ChannelMessageSendEmbed(m.ChannelID, embed)
+		return err
 	}
 
-	_, err = s.ChannelMessageSendEmbed(m.ChannelID, embed)
+	// If we got a ball, we doing an specific check against a pokemon. First
+	// sure we did get a pokemon
+	_, err = s.ChannelMessage(m.ChannelID, cleanPkmName+" "+ball.Name)
 	return err
 }
 
-func (b *Bot) getPokemonTopFourBalls(pokemonName string, shiny bool) (*discordgo.MessageEmbed, error) {
+func (b *Bot) getPokemonTopFourBalls(pokemonName, form string, shiny bool) (*discordgo.MessageEmbed, error) {
 	pkm, err := b.pokemonRepo.pokemon(pokemonName)
 	if err != nil {
 		return nil, botError{
 			title:   "Not Found",
-			details: fmt.Sprintf("%s was not found", pokemonName),
+			details: fmt.Sprintf("Pokemon %s was not found", pokemonName),
 		}
 	}
 
+	form = getSpriteForm(form)
 	name := pkm.Name
-	if pkm.isGigantamax() {
+	if form == gigantamax {
 		name = "G-Max " + name
 	}
 
 	embed := b.newEmbed()
 	embed.Title = "Best Catch Rates"
-	embed.Description = fmt.Sprintf("The best balls for catching G-Max %s are:", name)
+	embed.Description = fmt.Sprintf("The best balls for catching %s are:", name)
 	// todo(hector) add support for color
 
 	embed.Image = &discordgo.MessageEmbedImage{
-		// todo(hector) add form
-		URL: pkm.spriteImage(shiny, ""),
+		URL: pkm.spriteImage(shiny, form),
 	}
 
 	return embed, nil
+}
+
+func getFormFromArgs(args []string) string {
+
+	// the form will always be the second element
+	// e.g: blastoise gmax
+	if len(args) < 2 || args[1] == "" {
+		return ""
+	}
+
+	return getSpriteForm(args[1])
 }
